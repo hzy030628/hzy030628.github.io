e029c05b5f076fb54ad629f4b1d1cb414b24df06
hzy030628 <2596328958@qq.com>
2023-03-14 11:07:23 +0800

    Site updated: 2023-03-14 11:07:21

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     audio: [
 	    {
         name: "春风十里报新年",
